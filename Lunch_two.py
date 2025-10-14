05b133a4b122832bc09e69e15b6f5aebaec4f4ac
qianxinsoong <qsoong@micron.com>
2025-10-14 09:49:35 +0800

    Update Lunch_two.py

--- Lunch_two.py
+++ Lunch_two.py
@@ -176,7 +176,7 @@ df_votes = pd.DataFrame(st.session_state.lunch_options)
 chart = alt.Chart(df_votes).mark_bar().encode(
     x=alt.X('name', sort='-y', title='Restaurant'),
     y=alt.Y('votes', title='Votes'),
-    color='Theme'
+    color='theme'
 ).properties(width=600, height=400)
 st.altair_chart(chart, use_container_width=True)
 
